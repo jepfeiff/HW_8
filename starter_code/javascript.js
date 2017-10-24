41a91351bbaf903ffcae3afea3d1457c9dca6859
jepfeiff <jepfeiff@gmail.com>
2017-10-23 17:00:00 -0700

    HW_8
    This is homework 8 - not great - but i think i satisfied the basics

---
+++ starter_code/javascript.js
@@ -0,0 +1,21 @@
+console.log('file loading');
+
+$(document).ready(function() {
+	console.log ("load page")
+
+$("#register").submit (function(){
+
+var fullname = $(“#fullname”).val();
+var phone = $(“#phone”).val();
+var email = $(“#email”).val();
+
+if (fullname.length > 0 &&
+   phonenumber.length > 0 &&
+   email.length > 0) {
+	// Do submission
+} else {
+	// Do error
+}
+
+
+});
